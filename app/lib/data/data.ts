06ea98c1e2d4f97a58a7f5051b4cd9fc53a9362c
pcoellar <pedrocoellarburbano@hotmail.com>
2024-06-18 17:02:03 -0500

    improve api times

--- app/lib/data/data.ts
+++ app/lib/data/data.ts
@@ -7,7 +7,7 @@ export async function fetchTopicsSummary(category: string, location: string) {
     const data: TopicSummary[] = await new Promise<TopicSummary[]>(resolve => {
         setTimeout(() => {
             resolve(topicsSummary);
-        }, 2000);
+        }, 1500);
     });
     return data;
 }
@@ -17,7 +17,7 @@ export async function fetchLocations(category: string, location: string) {
     const data: Location[] = await new Promise<Location[]>(resolve => {
         setTimeout(() => {
             resolve(locations);
-        }, 1000);
+        }, 800);
     });
     return data;
 }
@@ -26,7 +26,7 @@ export async function fetchClientReviews(category: string, location: string) {
     const data: ClientReview[] = await new Promise<ClientReview[]>(resolve => {
         setTimeout(() => {
             resolve(clientReviews);
-        }, 1000);
+        }, 500);
     });
     return data;
 }
@@ -36,7 +36,7 @@ export async function fetchStatistics(category: string, location: string) {
     const data: Statistic[] = await new Promise<Statistic[]>(resolve => {
         setTimeout(() => {
             resolve(statistics);
-        }, 2000);
+        }, 1500);
     });
     return data;
 }
@@ -46,7 +46,7 @@ export async function fetchBlogs(category: string, location: string) {
     const data: Blog[] = await new Promise<Blog[]>(resolve => {
         setTimeout(() => {
             resolve(blogs);
-        }, 1000);
+        }, 800);
     });
     return data;
 }
